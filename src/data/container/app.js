12e466495b58350bcfe5463cc439239ec10f841d
Toby Brancher <email@tobybrancher.net>
2021-06-09 17:26:14 +0100

    Fix sorting of last online This was quite a large task as it involved moving the axios query for individual members into our App data container. It also required us to loop through every record and run asynchronous API requests, waiting for them all to complete, before then updating our interface.

--- src/data/container/app.js
+++ src/data/container/app.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import App from "components/App";
+import axios from "axios";
 import useAxios from "axios-hooks";
-
-// /GroupV2/{groupId}/Members/
-// https://www.bungie.net/Platform/
 
 const API_ROOT = "https://www.bungie.net/Platform/";
 const GROUP_ID = 3845745;
 const API_KEY = "c98d45d4434546bfaa5ad45464b86230";
+
+function filterProfileData(member) {
+    const lastOnline = member.profile.data.dateLastPlayed;
+
+    return {
+        lastOnline
+    };
+}
 
 function filterData(member) {
     const name = member.destinyUserInfo.LastSeenDisplayName;
@@ -30,22 +36,57 @@ function filterData(member) {
 }
 
 function AppContainer() {
-    const [{ data, loading, error }] = useAxios({
+    const [combinedMembersData, setCombinedMembersData] = useState([]);
+    const [
+        { data: clanData, loading: clanLoading, error: clanError }
+    ] = useAxios({
         url: `${API_ROOT}GroupV2/${GROUP_ID}/Members/`,
         headers: {
             "X-API-Key": API_KEY
         }
     });
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error!</p>;
+    useEffect(() => {
+        if (!clanData) return false;
+        const filteredMemberData = clanData.Response.results.map(filterData);
+        const combineMemberData = filteredMemberData.map((member) => {
+            return axios({
+                method: "GET",
+                url: `${API_ROOT}Destiny2/${member.destinyMemberType}/Profile/${member.destinyId}/?components=100,200`,
+                headers: {
+                    "X-API-Key": API_KEY
+                }
+            })
+                .then((res) => {
+                    const combinedData = {
+                        ...member,
+                        ...filterProfileData(res.data.Response)
+                    };
 
-    // Tidy data
-    const members = data.Response.results.map(filterData);
+                    return combinedData;
+                })
+                .catch((e) => console.error(e));
+        });
 
-    // console.log(members);
+        Promise.all(combineMemberData).then((combinedData) => {
+            setCombinedMembersData(combinedData);
+        });
+    }, [clanData, setCombinedMembersData]);
 
-    return <App members={members} memberCount={members.length} />;
+    if (!clanLoading) {
+        if (clanError) return <p>Error!</p>;
+        if (combinedMembersData.length > 0) {
+            return (
+                <App
+                    members={combinedMembersData}
+                    memberCount={combinedMembersData.length}
+                />
+            );
+        }
+        return <p>Loading...</p>;
+    }
+
+    return <p>Loading...</p>;
 }
 
 export default AppContainer;
